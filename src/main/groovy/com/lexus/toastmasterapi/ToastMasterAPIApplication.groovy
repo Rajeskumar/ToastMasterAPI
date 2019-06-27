7b92947fa86f13f12e25188e8ce2b80c98b96949
Rajeshkumar Ganesan <rajeskumarcse@gmail.com>
2019-06-26 20:10:22 -0500

    Version 2.0 Commit  - Cleanup

--- src/main/groovy/com/lexus/toastmasterapi/ToastMasterAPIApplication.groovy
+++ src/main/groovy/com/lexus/toastmasterapi/ToastMasterAPIApplication.groovy
@@ -3,12 +3,18 @@ package com.lexus.toastmasterapi
 import com.lexus.toastmasterapi.controller.APIController
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class ToastMasterAPIApplication {
+class ToastMasterAPIApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        application.sources(ToastMasterAPIApplication.class)
+    }
 
     static void main(String[] args) {
         SpringApplication.run(APIController, args)
     }
-
 }
